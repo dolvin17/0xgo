4cff36545758522cf1141bd68bfe21fa2bddf5e0
dolvin17 <huertas876@gmail.com>
2024-02-03 11:39:07 +0100

    styling footer

--- src/components/Navigation/Footer.js
+++ src/components/Navigation/Footer.js
@@ -3,33 +3,31 @@ import Image from "next/image"
 export default function Footer() {
 	return (
 		<>
-		<div className="flex flex-row items-center justify-between px-6 py-4 mx-16 mb-4 border-b-2 border-0xgo-dark ">
-		  <div className="flex items-center gap-4">
-		  <a className="flex-shrink-0 max-w-[5rem]" href="/"> 
-			<Image src="/logohor.svg" alt="0xGO" width={73} height={73} />
+		<div className="flex flex-row items-center justify-between px-6 py-4 mx-12 mb-4 border-b-2 sm:flex-wrap border-0xgo-dark ">
+		  <div className="flex items-center gap-4 sm:flex-col">
+		  <a className="flex-shrink-0 " href="/"> 
+			<Image className="max-w-[120px] px-2" src="/logohor.svg" alt="0xGO" width={73} height={73} />
 		  </a>
 		  </div>
 		  <div className="flex items-center gap-4">
-			<div className="flex-grow"></div>
-			<button className="flex items-center gap-4 leading-tight">
+			<button className="flex items-center gap-4 text-xs leading-tight">
 			 Cartesi Proposal
 		  </button>
-		  <button className="flex items-center gap-4 leading-tight">
+		  <button className="flex items-center gap-4 text-xs leading-tight">
 			 About Us
 		  </button>
-		  <button className="flex items-center gap-4 leading-tight">
+		  <button className="flex items-center gap-4 text-xs leading-tight">
 			 Docs
 		  </button>
-		  <button className="flex items-center gap-4 leading-tight">
+		  <button className="flex items-center gap-4 text-xs leading-tight">
 			 FAQ
 		  </button>
 		  </div>
 		</div>
-		<div className="flex flex-row items-start justify-start gap-3 px-6 mx-16 mb-8 ">
-		<button className="flex items-center gap-4 leading-tight text-white/40"> CharmVerse</button>
-		<button className="flex items-center gap-4 leading-tight text-white/40"> Twitter</button>
-		<button className="flex items-center gap-4 leading-tight text-white/40"> Telegram</button>
-		<div className="flex-grow gap-4"></div>
+		<div className="flex flex-row items-center justify-center gap-3 mx-16 mb-8">
+		<button className="flex items-center gap-4 text-xs leading-tight text-white/40"> CharmVerse</button>
+		<button className="flex items-center gap-4 text-xs leading-tight text-white/40"> Twitter</button>
+		<button className="flex items-center gap-4 text-xs leading-tight text-white/40"> Telegram</button>
 		</div>
 
 		</>
