906de5700d3862f0caef2ac35d9156a48e3a0625
dolvin17 <huertas876@gmail.com>
2024-02-03 05:26:06 +0100

    adding favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,12 +3,13 @@ import { Web3Modal } from "@/context/Web3Modal";
 
 export const metadata = {
   title: "Oxgo",
-  description: "LFG",
+  description: "A Cartesi Project",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+	<link rel="icon" type="image/svg+xml" href="/src/favicon.svg" />
 		<body>
 		<Web3Modal>{children}</Web3Modal>
 		</body>
