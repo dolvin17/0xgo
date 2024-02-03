99a1c825540f6bc206e93905df45b64fbc8f8f5b
dolvin17 <huertas876@gmail.com>
2024-02-03 13:40:52 +0100

    adding some features

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 export default function Navigation() {
   return (
-	<div className="flex flex-row items-center justify-between w-full px-6 py-4 mb-24 bg-0xgo-dark">
+	<div className="fixed top-0 flex flex-row items-center bg-opacity-50 justify-between w-full px-6 bg-0xgo-dark h-[60px] ">
 	  <div className="flex items-center gap-4">
 	  <a className="flex-shrink-0 max-w-[5rem]" href="/"> 
 		<Image src="/logo.svg" alt="0xGO" width={42} height={42} />
--- src/components/Navigation/Resume.js
+++ src/components/Navigation/Resume.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Resume() {
 	  return (
-	<div className="flex items-center justify-center gap-8 my-16 sm:text-xl">
+	<div className="flex items-center justify-center gap-8 my-16 mt-32 sm:text-xl">
 		<div className="flex flex-col items-center">
 		<span className="font-bold">10,000 CTSI</span>
 		<span className="font-thin">Total Staked</span>
