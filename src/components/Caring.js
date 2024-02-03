87b172e8a07dc8505302080e379be6f6873c059b
dolvin17 <huertas876@gmail.com>
2024-02-03 11:48:43 +0100

    responsive

--- src/components/Caring.js
+++ src/components/Caring.js
@@ -5,7 +5,7 @@ export default function Caring() {
   return (
     <>
       <a className="flex-shrink-0" href="/">
-        <div className="flex flex-row w-full max-w-xl gap-8 p-6 mx-auto my-8 border-opacity-50 bg-0xgo-dark rounded-2xl shadow-black/30">
+        <div className="flex flex-row max-w-xl gap-8 p-6 mx-1 my-8 border-opacity-50 w-fit sm:mx-auto bg-0xgo-dark rounded-2xl shadow-black/30">
           <Image src="/caring.svg" alt="caring" width={82} height={82} />
           <div className="flex flex-col text-white">
             <span className="text-xl font-semibold">Thanks for caring</span>
