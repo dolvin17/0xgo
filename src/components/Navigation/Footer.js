3a870ba9db4025cc04b2f8466fd22491a51840c7
dolvin17 <huertas876@gmail.com>
2024-02-06 03:24:42 +0100

    adding links for social

--- src/components/Navigation/Footer.js
+++ src/components/Navigation/Footer.js
@@ -1,36 +1,66 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Footer() {
-	return (
-		<>
-		<div className="flex flex-row items-center justify-between px-6 py-4 mx-12 mb-4 border-b-2 sm:flex-wrap border-0xgo-dark ">
-		  <div className="flex items-center gap-4 sm:flex-col">
-		  <a className="flex-shrink-0 " href="/"> 
-			<Image className="max-w-[120px] px-2" src="/logohor.svg" alt="0xGO" width={73} height={73} />
-		  </a>
-		  </div>
-		  <div className="flex items-center gap-4">
-			<button className="flex items-center gap-4 text-xs leading-tight">
-			 Cartesi Proposal
-		  </button>
-		  <button className="flex items-center gap-4 text-xs leading-tight">
-			 About Us
-		  </button>
-		  <button className="flex items-center gap-4 text-xs leading-tight">
-			 Docs
-		  </button>
-		  <button className="flex items-center gap-4 text-xs leading-tight">
-			 FAQ
-		  </button>
-		  </div>
-		</div>
-		<div className="flex flex-row items-center justify-center gap-3 mx-16 mb-8">
-		<button className="flex items-center gap-4 text-xs leading-tight text-white/40"> CharmVerse</button>
-		<Link href="https://twitter.com/0xgo_fund" target="_blank" className="flex items-center gap-4 text-xs leading-tight text-white/40"> Twitter</Link>
-		<button className="flex items-center gap-4 text-xs leading-tight text-white/40"> Telegram</button>
-		</div>
-
-		</>
-	)
+  return (
+    <>
+      <div className="flex flex-row items-center justify-between px-6 py-4 mx-12 mb-4 border-b-2 sm:flex-wrap border-0xgo-dark ">
+        <div className="flex items-center gap-4 sm:flex-col">
+          <a className="flex-shrink-0 " href="/">
+            <Image
+              className="max-w-[120px] px-2"
+              src="/logohor.svg"
+              alt="0xGO"
+              width={73}
+              height={73}
+            />
+          </a>
+        </div>
+        <div className="flex items-center gap-4">
+          <button className="flex items-center gap-4 text-xs leading-tight">
+            Cartesi Proposal
+          </button>
+          <Link
+            href="/about"
+            target="_blank"
+            className="flex items-center gap-4 text-xs leading-tight"
+          >
+            About Us
+          </Link>
+          <Link
+            target="_blank"
+            href="https://0xgo-fund.gitbook.io/docs/"
+            className="flex items-center gap-4 text-xs leading-tight"
+          >
+            Docs
+          </Link>
+          <button className="flex items-center gap-4 text-xs leading-tight">
+            FAQ
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-row items-center justify-center gap-3 mx-16 mb-8">
+        <button className="flex items-center gap-4 text-xs leading-tight text-white/40">
+          {" "}
+          CharmVerse
+        </button>
+        <Link
+          href="https://twitter.com/0xgo_fund"
+          target="_blank"
+          className="flex items-center gap-4 text-xs leading-tight text-white/40"
+        >
+          {" "}
+          Twitter
+        </Link>
+        <Link
+          target="a_blank"
+          href="https://t.me/+XXvIHa1l_K9mY2Fk"
+          className="flex items-center gap-4 text-xs leading-tight text-white/40"
+        >
+          {" "}
+          Telegram
+        </Link>
+      </div>
+    </>
+  );
 }
