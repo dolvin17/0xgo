5b181686033c6193f4cc1c2df1327d3c2d825981
dolvin17 <huertas876@gmail.com>
2024-02-15 22:57:17 +0100

    adding css use card

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,21 +7,35 @@ module.exports = {
   ],
   theme: {
     extend: {
-	  colors: {
-		'0xgo': {
-			dark: '#1D1D1D',
-			blue: '#02F6FF',
-			green: '#00E834'
-		}
+      keyframes: {
+		aniatedgradient: {
+			'0%': { backgroundPosition: '0% 50%' },
+			'50%': { backgroundPosition: '100% 50%' },
+			'100%': { backgroundPosition: '0% 50%' },
+		},
 	  },
+      fontFamily: {
+        "0xgo": ["Roboto", "sans-serif"],
+      },
+      colors: {
+        "0xgo": {
+          dark: "#1D1D1D",
+          blue: "#02F6FF",
+          green: "#00E834",
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+	  backgroundSize: {
+        '300%': '300%',
+      },
+	  animation: {
+        gradient: 'animatedgradient 6s ease infinite alternate',
+	}
     },
   },
-  plugins: [
-	require('tailwindcss-animated')
-  ],
+  plugins: [require("tailwindcss-animated")],
 };
