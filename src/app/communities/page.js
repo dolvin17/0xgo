5ca030c7a2dd7593629923f3e7070cdfeb920a34
dolvin17 <huertas876@gmail.com>
2024-02-16 21:37:38 +0100

    adding navbar and footer

--- src/app/communities/page.js
+++ src/app/communities/page.js
@@ -1,11 +1,15 @@
 import Link from "next/link";
 import Image from "next/image";
 import Projects from "../../components/Community/Projects";
+import Navigation from "@/components/Navigation/Navigation";
+import Footer from "@/components/Navigation/Footer";
 
 export default function Communities() {
 	return (
 		<>
+		<Navigation />
 		<Projects />
+		<Footer />
 		</>
 	)
 }
